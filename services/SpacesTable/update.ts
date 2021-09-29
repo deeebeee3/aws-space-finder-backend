86cb487987e2c7244c51eaf92a349bd1121043d2
Deepak <deeebeee3@hotmail.com>
2021-09-29 10:46:58 +0100

    improvement - wrap everything in try catch

--- services/SpacesTable/update.ts
+++ services/SpacesTable/update.ts
@@ -26,28 +26,32 @@ async function handler(
 
   const spaceId = event.queryStringParameters?.[PRIMARY_KEY];
 
-  if (requestBody && spaceId) {
-    const requestBodyKey = Object.keys(requestBody)[0];
-    const requestBodyvalue = requestBody[requestBodyKey];
+  try {
+    if (requestBody && spaceId) {
+      const requestBodyKey = Object.keys(requestBody)[0];
+      const requestBodyvalue = requestBody[requestBodyKey];
 
-    const updateResult = await dbClient
-      .update({
-        TableName: TABLE_NAME,
-        Key: {
-          [PRIMARY_KEY]: spaceId, //row
-        },
-        UpdateExpression: "set #zzzNew = :new",
-        ExpressionAttributeNames: {
-          "#zzzNew": requestBodyKey, //column
-        },
-        ExpressionAttributeValues: {
-          ":new": requestBodyvalue, //value
-        },
-        ReturnValues: "UPDATED_NEW",
-      })
-      .promise();
+      const updateResult = await dbClient
+        .update({
+          TableName: TABLE_NAME,
+          Key: {
+            [PRIMARY_KEY]: spaceId, //row
+          },
+          UpdateExpression: "set #zzzNew = :new",
+          ExpressionAttributeNames: {
+            "#zzzNew": requestBodyKey, //column
+          },
+          ExpressionAttributeValues: {
+            ":new": requestBodyvalue, //value
+          },
+          ReturnValues: "UPDATED_NEW",
+        })
+        .promise();
 
-    result.body = JSON.stringify(updateResult);
+      result.body = JSON.stringify(updateResult);
+    }
+  } catch (error: any) {
+    result.body = error.message;
   }
 
   return result;
